5f204d996bfe36d277b03e252842403fab3c24fb
olington <ola.olupeka@gmail.com>
2023-07-17 19:51:29 +0930

    final pypoll commit hopefully

--- Pypoll/Main.py
+++ Pypoll/Main.py
@@ -5,6 +5,11 @@ import csv
 #path
 csvpath = os.path.join("Resources", "election_data.csv")
 
+#variables
+Charles_Casper_Stockham_count = 0
+Diana_DeGette_count = 0
+Raymon_Anthony_Doane_count = 0
+
 #open file
 with open(csvpath) as csvfile:    
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -12,26 +17,53 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     print(f"Header: {csv_header}")
 
-    voter_information = csvfile
-   
-    ballot_id_count = int(voter_information[0])    
-    Charles_Casper_Stockham_count = 0
-    Diana_Degette_count = 0
-    Raymon_Anthony_Doane_count = 0
+    voter_information = csvfile   
 
-    total_votes = len(ballot_id_count)
-    print(total_votes)
+
     # set up what we want to look for
     candidate_list = {}
+    row_count = 0
+    
+    for row in csvreader:
+        #print(row[2]) and get candidate votes
+        if row[2] == "Charles Casper Stockham":
+            Charles_Casper_Stockham_count += 1
+        elif row[2] == "Diana DeGette":
+            Diana_DeGette_count += 1
+        elif row[2] == "Raymon Anthony Doane":
+            Raymon_Anthony_Doane_count += 1
+        
+        row_count += 1    
+#print(row_count)        
+# print(Charles_Casper_Stockham_count)
+# print(Diana_DeGette_count)
+# print(Raymon_Anthony_Doane_count)
 
+Charles_Casper_Stockham_percentage = (Charles_Casper_Stockham_count / row_count) * 100
+Diana_DeGette_percentage = (Diana_DeGette_count / row_count) * 100
+Raymon_Anthony_Doane_percentage = (Raymon_Anthony_Doane_count / row_count) * 100
 
-    # loop through each row of the data set
-# for row in csvreader:
+print(Charles_Casper_Stockham_percentage)
+print(Diana_DeGette_percentage)
+print(Raymon_Anthony_Doane_percentage)
 
-   
-    # if row[2] == "Charles_Casper_Stockham":
-    #     Charles_Casper_Stockham_count += 1
-    # elif row[2] == "Diana Degette":
-    #     Diana_Degette_count += 1
-    # elif row[2] == "Raymon Anthony Doane":
-    #     Raymon_Anthony_Doane_count += 1
+#result summary
+output = (f"Election Results\n"
+    f"---------------------------------------------------------------------------\n"
+    f"Total Votes: {row_count}\n"
+    f"---------------------------------------------------------------------------\n"
+    f"Charles Casper Stockham: {Charles_Casper_Stockham_percentage: .2f}% ({Charles_Casper_Stockham_count})\n"
+    f"Diana DeGette: {Diana_DeGette_percentage: .2f}% ({Diana_DeGette_count})\n"
+    f"Raymon Anthony Doane: {Raymon_Anthony_Doane_percentage: .2f}% ({Raymon_Anthony_Doane_count})\n"
+    f"---------------------------------------------------------------------------\n"
+    f"Winner : Diana DeGette\n"
+    f"---------------------------------------------------------------------------\n")       
+
+print(output)    
+
+file_to_save = os.path.join("Analysis", "output_file.txt")
+
+#Export as txt.file
+with open(file_to_save, "w") as txt_file:
+    txt_file.write(output)
+    
